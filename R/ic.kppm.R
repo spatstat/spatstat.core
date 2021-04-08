47b5d717e97c22be246e76058ad97d365d05a74f
Rasmus Waagepetersen <rw@math.aau.dk>
2021-04-08 21:13:26 +0200

    Corrected versions of ic.kppm.R and ic.kppm.Rd

--- R/ic.kppm.R
+++ R/ic.kppm.R
@@ -1,13 +1,22 @@
-ic.kppm <- function(object){
-  loglike<-object$loglike
-  betahat<-object$betahat
-  n <- object$n
-  p <-sum(betahat!=0)
-  if (is.poisson(object$obj.kppm))
+ic.kppm <- function(object,X){
+  
+  if (is.poisson(object)){
+    loglike<-object$maxlogpl
+    betahat<-object$coef
+    n=X$n
+  }
+  else {
+    loglike<-object$po$maxlogpl
+    betahat<-object$po$coef
+    n <- object$X$n
+  } 
+  p <-length(betahat)
+
+  if (is.poisson(object))
     df<-p
   else {
-    verifyclass(object$obj.kppm, "kppm")
-    cov<-vcov.kppm(object$obj.kppm,what="internals")
+    verifyclass(object, "kppm")
+    cov<-vcov.kppm(object,what="internals")
     df<-p+sum(diag(as.matrix(cov$J.inv%*%cov$E))) #compute p_approx
   }
 
@@ -15,5 +24,5 @@ ic.kppm <- function(object){
   cic = -2*loglike+df*2
   #NB: cbic is BIC and cic is AIC in case of Poisson process
   
-  return(list(loglike=loglike,cbic=cbic,cic=cic))
+  return(list(loglike=loglike,cbic=cbic,cic=cic,df=df))
 }
