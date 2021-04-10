37a412e8a013412a73f20c4f1b30f49d16dc7cc4
Adrian Baddeley <adrian.baddeley@uwa.edu.au>
2021-04-10 11:05:38 +0800

    New generic 'ic'

--- R/ic.kppm.R
+++ R/ic.kppm.R
@@ -1,28 +1,37 @@
-ic.kppm <- function(object,X){
-  
-  if (is.poisson(object)){
-    loglike<-object$maxlogpl
-    betahat<-object$coef
-    n=X$n
-  }
-  else {
-    loglike<-object$po$maxlogpl
-    betahat<-object$po$coef
-    n <- object$X$n
-  } 
-  p <-length(betahat)
+#'
+#'   ic.kppm.R
+#'
+#'   Original by Rasmus Waagepetersen, 10 april 2021
+#'
+#'   Tweaks by Adrian Baddeley
 
-  if (is.poisson(object))
-    df<-p
-  else {
-    verifyclass(object, "kppm")
-    cov<-vcov.kppm(object,what="internals")
-    df<-p+sum(diag(as.matrix(cov$J.inv%*%cov$E))) #compute p_approx
-  }
+ic <- function(object) { UseMethod("ic") }
 
+ic.ppm <- function(object) {
+  loglike <- logLik(object)
+  ll <- as.numeric(loglike)
+  df <- attr(loglike, "df")
+  betahat <- coef(object)
+  n <- npoints(data.ppm(object))
+  cbic <- -2*ll+df*log(n)
+  cic  <- -2*ll+df*2
+  ## cbic is BIC and cic is AIC in case of Poisson process
+  return(list(loglike=ll, cbic=cbic, cic=cic, df=df))
+}
+
+ic.kppm <- function(object){
+  po <- as.ppm(object)
+  loglike <- logLik(po)
+  ll <- as.numeric(loglike)
+  betahat <- coef(object)
+  p <- length(betahat)
+  n <- npoints(data.ppm(po))
+  co <- vcov(object, what="internals")
+  df <- p + sum(diag(as.matrix(co$J.inv %*% co$E))) #compute p_approx
   cbic = -2*loglike+df*log(n)
   cic = -2*loglike+df*2
-  #NB: cbic is BIC and cic is AIC in case of Poisson process
-  
-  return(list(loglike=loglike,cbic=cbic,cic=cic,df=df))
+  cbic <- -2*ll+df*log(n)
+  cic  <- -2*ll+df*2
+  ## cbic is BIC and cic is AIC in case of Poisson process
+  return(list(loglike=ll, cbic=cbic, cic=cic, df=df))
 }
